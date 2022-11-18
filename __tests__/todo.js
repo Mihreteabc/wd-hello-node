45a4050c3315628f80e9c1f4e7164528d1c7cab8
Mihreteab Chernet <98508474+Mihreteabc@users.noreply.github.com>
2022-11-18 23:56:35 +0530

    Update todo.js

--- __tests__/todo.js
+++ __tests__/todo.js
@@ -10,38 +10,43 @@ const today = formattedDate(todayDate);
 const yesterday = formattedDate(
   new Date(new Date().setDate(todayDate.getDate() - 1))
 );
+
 const tomorrow = formattedDate(
-  new Date(new Date().setDate(todayDate.getDate() + 1))
+new Date(new Date().setDate(todayDate.getDate() + 1))
 );
 const { all, markAsComplete, add, overdue, dueToday, dueLater } = todoList();
 
 describe("TodoList Test Suite", () => {
   beforeAll(() => {
     add({
-      title: "todo1",
+      title: "todo_1",
       completed: false,
       dueDate: today,
     });
+    
     add({
-      title: "todo2",
+      title: "todo_2",
       completed: false,
       dueDate: yesterday,
     });
+    
     add({
-      title: "todo3",
+      title: "todo_3",
       completed: false,
       dueDate: tomorrow,
     });
   });
+  
   test("add", () => {
     const count = all.length;
     add({
-      title: "todo4",
+      title: "todo_4",
       completed: true,
       dueDate: today,
     });
     expect(all.length).toBe(count + 1);
   });
+  
   test("markAsComplete", () => {
     expect(all[0].completed).toBe(false);
     markAsComplete(0);
@@ -53,12 +58,14 @@ describe("TodoList Test Suite", () => {
     expect(overdue_list.length).toBe(1);
     expect(overdue_list[0]).toBe(all[1]);
   });
+  
   test("dueToday_items", () => {
     let dueToday_list = dueToday();
     expect(dueToday_list.length).toBe(2);
     expect(dueToday_list[0]).toBe(all[0]);
     expect(dueToday_list[1]).toBe(all[3]);
   });
+  
   test("dueLater_items", () => {
     let dueLater_list = dueLater();
     expect(dueLater_list.length).toBe(1);
